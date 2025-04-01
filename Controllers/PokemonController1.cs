2fb96b4a8bb60997cdf82a49acdadf73f78d5d9f
Erick Schaedler <desenv10@idata.com.br>
2025-04-01 07:13:29 -0300

    feat: pc1

---
+++ Controllers/PokemonController1.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using API_POKEMON.Dtos;
+using System.Text.Json;
+
+namespace API_POKEMON.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PokemonController1 : ControllerBase
+{
+    public class APokemonResult
+    {
+        public string? name { get; set; }
+    }
+
+    public class AResponseGetPokemonInPokeApiDto
+    {
+        public List<APokemonResult>? results { get; set; }
+    }
+
+    [HttpGet]
+    [Route("pokemonsGroupedByColor/{quantity:int}")]
+    public async Task<ActionResult> GetPokemonsGroupedByColorAsync (int quantity)
+    {
+        try
+        {
+            if (quantity < 1 || quantity > 100)
+                throw new ArgumentException("Informe uma quantidade entre 1 e 100");
+
+            using var client = new HttpClient();
+            var baseApi = "https://pokeapi.co/api/v2/";
+
+            var response = await client.GetAsync($"{baseApi}pokemon?limit={quantity}&offset=0");
+            if(!response.IsSuccessStatusCode)
+                throw new HttpRequestException($"Erro ao consultar API de pokemons: {baseApi}");
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var deserializedContent = JsonSerializer.Deserialize<AResponseGetPokemonInPokeApiDto>(responseContent)
+                ?? throw new Exception("Erro ao deserializar resposta da API de pokemons");
+
+            var results = deserializedContent.results;
+
+            if(results == null || !results.Any())
+                throw new Exception("Objeto `results` que contém os pokemosn está inválido");
+
+            return Ok(results);
+        }
+        catch(ArgumentException aex)
+        {
+            return BadRequest(aex.Message);
+        }
+        catch(HttpRequestException hre)
+        {
+            return BadRequest(hre.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro interno ao consultar pokemons: {ex.Message}");
+        }
+    }
+}
