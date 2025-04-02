f0a52f0744fa6f80ae19d12da59d16e4f83e8562
Erick Schaedler <desenv10@idata.com.br>
2025-04-02 17:59:16 -0300

    feat: seleciona um bom codigo

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -1,110 +1,171 @@
-
 using Microsoft.AspNetCore.Mvc;
-using API_POKEMON.Dtos;
 using System.Text.Json;
 
 namespace API_POKEMON.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class PokemonController : ControllerBase
+public class PokemonController1 : ControllerBase
 {
-    [HttpGet("{quantityOfPokemons:int}")]
-    public async Task<ActionResult> GetPokemonsGroupByColorAsync(int quantityOfPokemons)
+    public class ResultDto
+    {
+        public string? name { get; set; }
+    }
+
+    public class ResponseGetPokemonsDto
+    {
+        public List<ResultDto>? results { get; set; }
+    }
+
+    public class ColorDto
+    {
+        public string? name { get; set; }
+    }
+
+    public class ResultGetSpecieDto
+    {
+        public ColorDto? color { get; set; }
+    }
+
+    public class PokemonsGroupedByColorDto
+    {
+        public string Color { get; set; } = string.Empty;
+        public List<string> Names { get; set; } = new List<string>();
+    }
+
+    [HttpGet]
+    [Route("pokemonsGroupedByColor/{quantity:int}")]
+    public async Task<ActionResult> GetPokemonsGroupedByColorAsync(int quantity)
     {
         var stopwatch = System.Diagnostics.Stopwatch.StartNew();
         try
         {
-            if (quantityOfPokemons == 0 || quantityOfPokemons > 100)
-                throw new ArgumentException("Quantidade de Pokemons deve ser entre 1 e 100.");
+            if (quantity < 1 || quantity > 100)
+                throw new ArgumentException("Informe uma quantidade entre 1 e 100");
 
             using var client = new HttpClient();
+            var baseApi = "https://pokeapi.co/api/v2/";
 
-            var response = await client.GetAsync($"https://pokeapi.co/api/v2/pokemon?limit={quantityOfPokemons}&offset=0");
+            var response = await client.GetAsync($"{baseApi}pokemon?limit={quantity}&offset=0");
             if (!response.IsSuccessStatusCode)
-                throw new HttpRequestException($"Erro ao consultar API de Pokemons: {response.ReasonPhrase}.");
+                throw new HttpRequestException($"Erro ao consultar API de pokemons: {baseApi}.");
 
-            string content = await response.Content.ReadAsStringAsync();
-            var deserializedContent = JsonSerializer.Deserialize<ResponseGetPokemonsDto>(content)
-                ?? throw new Exception("Erro ao ler resposta de pokeapi.");
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var deserializedContent = JsonSerializer.Deserialize<ResponseGetPokemonsDto>(responseContent)
+                ?? throw new Exception("Erro ao deserializar resposta da API de pokemons.");
 
             var results = deserializedContent.results;
 
             if (results == null || !results.Any())
-                throw new Exception("Objeto que contém os Pokemons inválido.");
+                throw new Exception("Objeto `results` que contém os pokemosn está inválido.");
 
-            var errors = new List<string>();
-            var listPokemonGroupedByColor = new List<PokemonGroupedByColorDto>();
-
-            foreach (var pokemon in results)
-            {
-                var pokemonName = pokemon.name;
-
-                if (string.IsNullOrWhiteSpace(pokemonName))
+            var tasks = results.Select<ResultDto, Task<(string? Name, string? Color, string? Error)>>(
+                async pokemon =>
                 {
-                    errors.Add("Pokemon com nome vazio.");
-                    continue;
-                }
+                    var pokemonName = pokemon.name;
+                    if (string.IsNullOrWhiteSpace(pokemonName))
+                        return (Name: null, Color: null, Error: "Pokemon com nome vazio.");
 
-                var responseSpecie = await client.GetAsync($"https://pokeapi.co/api/v2/pokemon-species/{pokemonName}");
-                if (!responseSpecie.IsSuccessStatusCode)
-                {
-                    errors.Add($"Erro ao consultar espécie do Pokemon {pokemonName}: {responseSpecie.ReasonPhrase}");
-                    continue;
-                }
-
-                string contentSpecie = await responseSpecie.Content.ReadAsStringAsync();
-                var deserializedContentSpecie = JsonSerializer.Deserialize<ResponseGetPokemonSpeciesDto>(contentSpecie)
-                    ?? throw new Exception("Erro ao ler resposta de pokeapi/species.");
-
-                var color = deserializedContentSpecie.color;
-
-                if (color == null)
-                {
-                    errors.Add($"Não foi possível identificar a cor do Pokemon: {pokemonName}");
-                    continue;
-                }
-
-                var colorName = color.name;
-
-                if (string.IsNullOrWhiteSpace(colorName))
-                {
-                    errors.Add($"Nome da cor do Pokemon {pokemonName} está vazia");
-                    continue;
-                }
-
-                var findListPokemonsByColor = listPokemonGroupedByColor.FirstOrDefault(x => x.Color == colorName);
-
-                if (findListPokemonsByColor == null)
-                {
-                    var newPokemonGroupedByColorDto = new PokemonGroupedByColorDto
+                    try
                     {
-                        Color = colorName,
-                        Names = new List<string>() { pokemonName }
-                    };
+                        var responseSpecie = await client.GetAsync($"{baseApi}pokemon-species/{pokemonName}");
+                        if (!responseSpecie.IsSuccessStatusCode)
+                            return (Name: pokemonName, Color: null, Error: $"Erro ao cunsultar espécie do pokemon: {pokemonName}.");
 
-                    listPokemonGroupedByColor.Add(newPokemonGroupedByColorDto);
+                        var responseContent = await responseSpecie.Content.ReadAsStringAsync();
+                        var deserializedSpecie = JsonSerializer.Deserialize<ResultGetSpecieDto>(responseContent);
+
+                        if (deserializedSpecie == null || deserializedSpecie.color == null)
+                            return (Name: pokemonName, Color: null, Error: $"Erro ao deserializar espécie do pokemon:{pokemonName}.");
+
+                        var colorName = deserializedSpecie.color.name;
+
+                        if (string.IsNullOrWhiteSpace(colorName))
+                            return (Name: pokemonName, Color: null, Error: "Cor com nome vazia.");
+
+                        return (Name: pokemonName, Color: colorName, Error: null);
+                    }
+                    catch (Exception ex)
+                    {
+                        return (Name: pokemonName, Color: null, Error: $"Erro ao buscar cor do pokemon {pokemonName}: {ex.Message}.");
+                    }
+                });
+
+            var pokemonResults = await Task.WhenAll(tasks);
+            var errors = new List<string>();
+            var listPokemonsGroupedByColor = new List<PokemonsGroupedByColorDto>();
+
+            foreach (var (Nome, Color, Error) in pokemonResults)
+            {
+                if (!string.IsNullOrWhiteSpace(Error))
+                {
+                    errors.Add(Error);
                 }
                 else
                 {
-                    findListPokemonsByColor.Names.Add(pokemonName);
+                    var pokemonColor = listPokemonsGroupedByColor.FirstOrDefault(x => x.Color == Color);
+
+                    if (pokemonColor != null)
+                    {
+                        pokemonColor.Names.Add(Nome!);
+                    }
+                    else
+                    {
+                        var newAokemonsColor = new PokemonsGroupedByColorDto
+                        {
+                            Color = Color!,
+                            Names = new List<string>() { Nome! },
+                        };
+
+                        listPokemonsGroupedByColor.Add(newAokemonsColor);
+                    }
                 }
             }
+
+            // ou
+
+            // var groupedDict = new Dictionary<string, List<string>>();
+
+            // foreach (var (Name, Color, Error) in pokemonsResults)
+            // {
+            //     if (!string.IsNullOrWhiteSpace(Error))
+            //     {
+            //         errors.Add(Error);
+            //     }
+            //     else
+            //     {
+            //         if (!groupedDict.ContainsKey(Color!))
+            //             groupedDict[Color!] = new List<string> { Name! };
+            //         else
+            //             groupedDict[Color!].Add(Name!);
+            //     }
+            // }
+
+            // var listPokemonGroupedByColor = groupedDict.Select(g => new PokemonsGroupedByColorDto
+            // {
+            //     Color = g.Key,
+            //     Names = g.Value
+            // }).ToList();
 
             stopwatch.Stop();
             var responseTime = stopwatch.ElapsedMilliseconds;
 
-            return Ok(new { listPokemonGroupedByColor, errors, responseTime });
+            return Ok(new { errors, listPokemonsGroupedByColor, responseTime });
+        }
+        catch (ArgumentException aex)
+        {
+            stopwatch.Stop();
+            return BadRequest(aex.Message);
+        }
+        catch (HttpRequestException hre)
+        {
+            stopwatch.Stop();
+            return BadRequest(hre.Message);
         }
         catch (Exception ex)
         {
             stopwatch.Stop();
-
-            return ex switch
-            {
-                ArgumentException => BadRequest(ex.Message),
-                _ => StatusCode(StatusCodes.Status500InternalServerError, $"Ocorreu um erro interno ao consultar os Pokemons: {ex.Message}")
-            };
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Erro interno ao consultar os pokemons: {ex.Message}");
         }
     }
 }
--- Dtos/PokemonDto.cs
+++ Dtos/PokemonDto.cs
@@ -1,27 +1,2 @@
 namespace API_POKEMON.Dtos;
 
-public class PokemonDto
-{
-    public string? name { get; set; }
-}
-
-public class ResponseGetPokemonsDto
-{
-    public List<PokemonDto>? results { get; set; }
-}
-
-public class PokemonGroupedByColorDto
-{
-    public string Color { get; set; } = string.Empty;
-    public List<string> Names { get; set; } = new List<string>();
-}
-
-public class PokemonColorDto
-{
-    public string? name { get; set; }
-}
-
-public class ResponseGetPokemonSpeciesDto
-{
-    public PokemonColorDto? color { get; set; }
-}
